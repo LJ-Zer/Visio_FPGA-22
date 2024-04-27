b45e1e912405cb1c646ed30df9310b330d632760
Zer-000 <20-06391@g.batstate-u.edu.ph>
2024-04-27 08:22:45 +0800

    change the frame size

--- Face_Detect/test_video_facedetect.cpp
+++ Face_Detect/test_video_facedetect.cpp
@@ -96,7 +96,7 @@ int main(int argc, char** argv) {
             fps_text << "FPS: " << std::fixed << std::setprecision(1) << fps;
             putText(frame, fps_text.str(), Point(10, 20), FONT_HERSHEY_SIMPLEX, 0.7, Scalar(0, 255, 0), 2);
 
-            resize(frame, frame, Size(120, 120));
+            resize(frame, frame, Size(320, 320));
             imshow("VisioAccelerAI", frame);
             if (waitKey(10) == 27) { // Exit on ESC key press
             break;
