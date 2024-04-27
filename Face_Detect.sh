dcb2a198f7ba28e7622720da2787049edb00b49f
Zer-000 <20-06391@g.batstate-u.edu.ph>
2024-04-27 09:36:46 +0800

    fix unloadapp

--- Face_Detect.sh
+++ Face_Detect.sh
@@ -1,4 +1,4 @@
-sudo xmutil unloadapps
+sudo xmutil unloadapp
 sudo xmutil loadapp kv260-nlp-smartvision
 cd Face_Detect
 /usr/bin/g++ -std=c++17 -I. -I/usr/include/opencv4 -o Visio-FD Visio-FD.cpp -lopencv_core -lopencv_video -lopencv_videoio -lopencv_imgproc -lopencv_imgcodecs -lopencv_highgui -lvitis_ai_library-facedetect -lvitis_ai_library-dpu_task -pthread -lglog
