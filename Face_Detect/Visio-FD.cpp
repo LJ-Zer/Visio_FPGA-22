5574bacd664d1ed601e5a45a61213f33bc1cfbeb
Zer-000 <20-06391@g.batstate-u.edu.ph>
2024-04-30 17:19:31 +0800

    1240x720 input-frame

--- Face_Detect/Visio-FD.cpp
+++ Face_Detect/Visio-FD.cpp
@@ -47,8 +47,8 @@ int main(int argc, char** argv) {
 
             // Resize for network input if necessary
             Mat resized_frame;
-            if (frame.cols != 2480 || frame.rows != 1440 {
-            resize(frame, resized_frame, Size(2480, 1440));
+            if (frame.cols != 1240 || frame.rows != 720) {
+            resize(frame, resized_frame, Size(1240, 720));
             } else {
             resized_frame = frame; // Avoid unnecessary copy if sizes match
             }
