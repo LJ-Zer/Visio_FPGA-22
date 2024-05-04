2b97aed40b8acb8adf857c000a28301f87ce9ce0
Zer-000 <20-06391@g.batstate-u.edu.ph>
2024-05-04 23:41:02 +0800

    placing of fps code line

--- Face_Detect/Visio-FD.cpp
+++ Face_Detect/Visio-FD.cpp
@@ -24,9 +24,6 @@ int main(int argc, char** argv) {
         return -1;
     }
 
-    cap.set(CAP_PROP_FRAME_WIDTH, 640);
-    cap.set(CAP_PROP_FRAME_HEIGHT, 360);
-
     int frame_width = cap.get(CAP_PROP_FRAME_WIDTH);
     int frame_height = cap.get(CAP_PROP_FRAME_HEIGHT);
 
